a9d2d7989eb2446c4db18458ddd676acab8d31cd
Daniel Nascimento Firmiano <daniel_2dfla@hotmail.com>
2022-03-11 20:57:58 -0300

    entrega de projeto

---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,40 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+// if(confirm("Quer iniciar uma nova rodada?")) {
+//       const boasVindas = ("Bem vindo(a) ao jogo de Blackjack!")
+// console.log(boasVindas)
+// const carta1 = comprarCarta(); 
+// const carta2 = comprarCarta();
+// let soma = carta1.valor + carta2.valor
+// console.log("Usuário - cartas :", carta1.texto, carta2.valor, "-" ,soma )
+// const carta3 = comprarCarta(); 
+// const carta4 = comprarCarta();
+// let soma2 = carta3.valor + carta4.valor
+// console.log("Computador - cartas :", carta3.texto, carta4.valor, "-" ,soma2 )
+// let condicao1 = soma>soma2
+// let condicao2 = soma<soma2
+// if (condicao1){
+//    console.log("O usuário ganhou!")
+// }else if (condicao2){
+//    console.log("O computador ganhou!")
+// }else{
+//    console.log("O jogo empatou!")
+// }
+
+//    } else {
+//       const nao = ("O jogo acabou")
+//      console.log(nao)
+//    } 
+
+
