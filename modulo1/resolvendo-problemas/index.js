4845fdd24e69ca7899457ddffd0922ff89da1ce0
Daniel Nascimento Firmiano <daniel_2dfla@hotmail.com>
2022-03-15 21:20:18 -0300

    Resumo de Resolvendo Problemas

---
+++ modulo1/resolvendo-problemas/index.js
@@ -0,0 +1,15 @@
+//*** Resumo dos 7 passos para resolver problemas ***
+
+//1 - Ter atenção no enunciado: Que serie entender o que o problema pede , saber identificar se tem alguma pegadinha ou algo que não esta muito claro.
+
+// 2 - Retirar informações relevantes: Procurar por pontos chaves dito no enunciado , que pode ser o "pulo do gato" pra resolver o problema.
+
+// 3 - Pensar em entradas e saídas: Retirar as informações que serão usadas como entradas e saber dizer as informações de saida , no caso , saber dizer qual é qual e a finalidade dela.
+
+// 4 - Fazer um esquema visual: Tentar vizualizar ele antes mesmo de começar a escreve-lo.
+
+// 5 - Criar código feio: Criar um código "provisório" para tentar dar os primeiros passos.
+
+// 6 - Testar força bruta: Testar esse código "provisório" e ver oque esta errado ou incompleto.
+
+// 7 - Refatorar: Dar o retoque final no teste, aquela caprichada , tanto pra melhorar a eficiencia dele e até mesmo a clareza.
