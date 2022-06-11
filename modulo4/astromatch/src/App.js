0ebb8f30acd9978f08e798b78aa6e2f4e24cd5a0
Daniel Nascimento Firmiano <daniel_2dfla@hotmail.com>
2022-06-10 23:39:48 -0300

    projeto-astromatch

---
+++ modulo4/astromatch/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css'
+import Perfil from "./components/Perfil";
+
+function App() {
+  return (
+    <div className="conteiner">
+      <Perfil/>
+    </div>
+  );
+}
+
+export default App;
